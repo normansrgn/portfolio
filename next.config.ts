a4b8d0d9cfe2f9c8b0bf7c4e739ffbd1cab0ef8b
norma <normanmagic2004@gmail.com>
2025-06-26 09:40:54 +0300

    $

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Отключить ESLint при сборке
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
